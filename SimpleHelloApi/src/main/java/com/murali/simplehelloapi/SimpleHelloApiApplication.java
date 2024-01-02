8b8ccd3946f26526eefa7bb762bcf127a4a17270
muralikumar27 <muralikumar62345@gmail.com>
2024-01-02 12:27:34 +0530

    JSON object response

--- SimpleHelloApi/src/main/java/com/murali/simplehelloapi/SimpleHelloApiApplication.java
+++ SimpleHelloApi/src/main/java/com/murali/simplehelloapi/SimpleHelloApiApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
-public class SimpleHelloApiApplication {
+import java.util.ArrayList;
+import java.util.List;
+
+
+/*public class SimpleHelloApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleHelloApiApplication.class, args);
@@ -18,4 +20,23 @@ public class SimpleHelloApiApplication {
         return "FIRST CODE IN SPRINGBOOT !!!";
     }
 
+}*/
+@SpringBootApplication
+@RestController
+public class SimpleHelloApiApplication{
+    public static void main(String[] args){
+        SpringApplication.run(SimpleHelloApiApplication.class,args);
+    }
+
+    @GetMapping("/display")
+    public Details display(){
+
+
+        return new Details(List.of("biriyani","barotta","fried chicken"),new Person("Murali"));
+    }
+
+    record Details(
+                   List<String>fav,
+                   Person person){}
+    record Person(String name){}
 }
