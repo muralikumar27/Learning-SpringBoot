dae8aa156e324650d23f6955b0d05b3b7fdf44cb
muralikumar27 <muralikumar62345@gmail.com>
2024-01-26 16:36:24 +0530

    Encrypted password and bypassed /register url using SecurityFilterChain refered spring documentation and youtube

--- basic-spring-security/src/main/java/com/murali/basicspringsecurity/service/UserServiceImplementation.java
+++ basic-spring-security/src/main/java/com/murali/basicspringsecurity/service/UserServiceImplementation.java
@@ -4,6 +4,7 @@ import com.murali.basicspringsecurity.entity.User;
 import com.murali.basicspringsecurity.model.UserModel;
 import com.murali.basicspringsecurity.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,13 +12,15 @@ public class UserServiceImplementation implements UserService{
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     public User registerUser(UserModel userModel) {
 
         User user = new User();
         user.setEmailId(userModel.getEmailId());
         user.setName(userModel.getName());
-        user.setPassword(userModel.getPassword());
+        user.setPassword(passwordEncoder.encode(userModel.getPassword()));
         user.setRole("user");
         return userRepository.save(user);
     }
